2421029539ecf911ddbca208bc6fe5d683a7dca6
Haozhe Yang <haozheyang@gmail.com>
2025-11-20 10:54:19 -0500

    update retail for HY

--- retail.py
+++ retail.py
@@ -3,57 +3,62 @@ from dotenv import load_dotenv
 import numpy as np
 import requests, asyncio, os
 
-polygon_key = os.getenv("POLYGON_KEY")
-
-class contract:
-    def __init__(self, name: str, frequency: float):
-        self.name = name
-        self.fair = None
-        self.frequency = frequency
+# class contract:
+#     def __init__(self, name: str, frequency: float):
+#         self.name = name
+#         self.fair = None
+#         self.frequency = frequency
     
-    async def trade(self, prices):
-        # wait time is inverse frequency
-        wait_time = np.random.exponential(scale=1.0 / self.frequency, size=100)
-        await asyncio.sleep(wait_time)
-        if not self.fair: return
+#     async def trade(self, prices):
+#         # wait time is inverse frequency
+#         wait_time = np.random.exponential(scale=1.0 / self.frequency)
+#         await asyncio.sleep(wait_time)
+#         if not self.fair: return
 
-        # flip a coin to decide side
-        side = np.random.choice([Side.Buy, Side.Sell])
-        price = prices[side]
-        if price and side == Side.Buy:
-            if price <= self.fair + 5:
-                await haorzhe.place_limit_order(market = self.name, side=Side.Buy, price=price, size=1, tif=Tif.Ioc)
-                print(f'Bought {self.name} for price {price}')
-        if price and side == Side.Sell:
-            if price >= self.fair - 5:
-                await haorzhe.place_limit_order(market = self.name, side=Side.Sell, price=price, size=1, tif=Tif.Ioc)
-                print(f'Sold {self.name} for price {price}')
+#         # flip a coin to decide side
+#         side = np.random.choice([Side.Buy, Side.Sell])
+#         price = prices[side]
+#         print(f'attempt to trade on contract {self.name} wait time = {wait_time} with fair = {self.fair} at price = {price}')
+#         if price and side == Side.Buy:
+#             if price <= self.fair + 5:
+#                 await haorzhe.place_limit_order(market = self.name, side=Side.Buy, price=price, size=1, tif=Tif.Ioc)
+#                 print(f'Bought {self.name} for price {price}')
+#         if price and side == Side.Sell:
+#             if price >= self.fair - 5:
+#                 await haorzhe.place_limit_order(market = self.name, side=Side.Sell, price=price, size=1, tif=Tif.Ioc)
+#                 print(f'Sold {self.name} for price {price}')
 
 ## Update the markets list to keep track of those markets.
 haorzhe = OracleClient()
-markets = ['tVIX5', 'tCMP']
-tVIX = contract(name='tVIX5', frequency=0.1)
-tCMP = contract(name='tCMP', frequency=0.1)
-contracts = [tVIX, tCMP]
+markets = ['HRVD', 'YALE', 'TIME', 'RAIN', 'TDS', 'PTS']
 
 async def trade_handler():
     while True:
-        snap = requests.get(f"https://api.polygon.io/v3/snapshot/indices", params={"ticker": "I:VIX", "apiKey": polygon_key}).json()
-        vix_meta = snap['results'][0]
-        tVIX.fair = int(vix_meta['value'] * 5.0)
-        tCMP.fair = 300 - tVIX.fair
+        # snap = requests.get(f"https://api.polygon.io/v3/snapshot/indices", params={"ticker": "I:VIX", "apiKey": 'g4OC2SA8TmEm9RBrD2UfAXnClXwdlHxf'}).json()
+        # vix_meta = snap['results'][0]
+        # tVIX.fair = int(vix_meta['value'] * 5.0)
+        # tCMP.fair = 300 - tVIX.fair
 
         book = haorzhe.get_book()
-        for contract in contracts:
-            contract_book = book[contract.name]
-            buys = contract_book['bids']
-            sells = contract_book['asks']
+        contract = np.random.choice(markets)
+        contract_book = book[contract]
+        buys = contract_book['bids']
+        sells = contract_book['asks']
 
-            best_buy = buys[0]['price'] if buys else None
-            best_sell = sells[0]['price'] if sells else None
+        best_buy = buys[0]['price'] if buys else None
+        best_sell = sells[0]['price'] if sells else None
 
-            # needs to flip them so that it's the other way
-            await contract.trade([best_sell, best_buy])        
+        side = np.random.choice([Side.Buy, Side.Sell])
+        if side == Side.Buy:
+            x = max(int(np.random.normal(3, 1)), 1)
+            print(f'buying {contract} at price {best_sell} for {x} quantity')
+            await haorzhe.place_limit_order(contract, side, best_sell, x, Tif.Ioc)
+        if side == Side.Sell:
+            x = max(int(np.random.normal(3, 1)), 1)
+            print(f'selling {contract} at price {best_buy} for {x} quantity')
+            await haorzhe.place_limit_order(contract, side, best_buy, x, Tif.Ioc)
+
+        await asyncio.sleep(0.5)
 
 ## ------------ DO NOT CHANGE BELOW THIS LINE ------------
 async def main():
@@ -64,7 +69,7 @@ async def main():
     await haorzhe.start_client(
         account=account_address,
         api_key=api_key,
-        domain="test"
+        domain="HarvardYale"
     )
     
     for market in markets:
